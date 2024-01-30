3d14110d62ec834ff3a2c81cc6ecfa61988ae681
karinahnatova <karinahnatova.catorce@alumnos.cei.es>
2024-01-30 22:06:57 +0100

    completado ej_03

--- Apuntes_NODE/Actividad_5/ej_03/index.js
+++ Apuntes_NODE/Actividad_5/ej_03/index.js
@@ -26,19 +26,48 @@ console.log(msg, "y la edad es: ", edad)
 // ej04: Deconstrucción de Array: Crea una función que tome un objeto de película, deconstruya sus propiedades para obtener titulo y director, y devuelva un array con estos dos valores.
 
 let pelicula = {
-    titulo: "elTitulo",
-    director: "elDirector"
+    titulo: "Batman",
+    director: "Christopher Nolan"
 
 }
 
 let unaPelicula = (pelicula) => {
     let {titulo, director} = pelicula
-    
+    let arraryPelicula = [titulo, director]
+    return (arraryPelicula)
     
 }
+console.log(unaPelicula(pelicula))
 
 // ej05: Deconstrucción de Datos del Clima: Crea una función que tome un objeto de datos de clima, deconstruya sus propiedades para obtener temperatura y humedad, y devuelva un array con estos dos valores.
+
+let datosClima = {
+    temperatura: "12 grados",
+    humedad: "70% humedad"
+}
+
+let tiempo = (datosClima) => {
+    const {temperatura, humedad} = datosClima
+    let arrayTiempo = [temperatura, humedad]
+    return(arrayTiempo)
+
+}
+console.log(tiempo(datosClima))
 
 // ej06: Deconstrucción de vectores: Crea una función llamada vectorAObjeto que tome un vector como argumento. Este vector contendrá información sobre una persona en el siguiente formato: persona = ["Juan", "Pérez", 30];
 // // Debe mostrar { nombre: "Juan", apellido: "Pérez", edad: 30 }
 
+const persona = ["Juan", "Perez", 30]
+
+
+let vectorAObjeto = (persona) => {
+    let [nombre, apellidos, edad] = persona
+    objetoPersona = {
+        nombre: nombre,
+        apellidos: apellidos,
+        edad: edad
+    }
+    return(objetoPersona)
+    
+}
+console.log(vectorAObjeto(persona))
